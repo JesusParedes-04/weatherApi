824ffe131bfb47858fb215918e08fc8f3f6e5305
Jesús Paredes <jjesus.paredesl@gmail.com>
2024-01-26 14:43:25 -0300

    errorhttp

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -10,40 +10,40 @@ const Resultado = () => {
 
     //Iconos weather map
 
-    const iconMappings = {
+    // const iconMappings = {
 
-        '01d': 'clear-day.svg',
-        '01n': 'clear-night.svg',
-        '02d': 'partly-cloudy-day.svg',
-        '02n': 'partly-cloudy-night.svg',
-        '03d': 'cloudy.svg',
-        '03n': 'cloudy.svg',
-        '04d': 'cloudy.svg',
-        '04n': 'cloudy.svg',
-        '09d': 'partly-cloudy-day-rain.svg',
-        '09n': 'partly-cloudy-night-rain.svg',
-        '10d': 'partly-cloudy-day-rain.svg',
-        '10n': 'partly-cloudy-night-rain.svg',
-        '11d': 'thunderstorms-day.svg',
-        '11n': 'thunderstorms-night.svg',
-        '13d': 'snow.svg',
-        '13n': 'partly-cloudy-night-snow.svg',
-        '50d': 'fog-day.svg',
-        '50n': 'fog-night.svg',
-    };
+    //     '01d': 'clear-day.svg',
+    //     '01n': 'clear-night.svg',
+    //     '02d': 'partly-cloudy-day.svg',
+    //     '02n': 'partly-cloudy-night.svg',
+    //     '03d': 'cloudy.svg',
+    //     '03n': 'cloudy.svg',
+    //     '04d': 'cloudy.svg',
+    //     '04n': 'cloudy.svg',
+    //     '09d': 'partly-cloudy-day-rain.svg',
+    //     '09n': 'partly-cloudy-night-rain.svg',
+    //     '10d': 'partly-cloudy-day-rain.svg',
+    //     '10n': 'partly-cloudy-night-rain.svg',
+    //     '11d': 'thunderstorms-day.svg',
+    //     '11n': 'thunderstorms-night.svg',
+    //     '13d': 'snow.svg',
+    //     '13n': 'partly-cloudy-night-snow.svg',
+    //     '50d': 'fog-day.svg',
+    //     '50n': 'fog-night.svg',
+    // };
 
-    const iconCode = weather && weather[0] && weather[0].icon;
-    const imageName = iconMappings[iconCode] || 'image.svg';
+    // const iconCode = weather && weather[0] && weather[0].icon;
+    // const imageName = iconMappings[iconCode] || 'image.svg';
 
 
     return (
 
         <div className="contenedor clima">
             <h2>El clima de {name} es: </h2>
-            <img className="imagen-clima"
+            {/* <img className="imagen-clima"
                 src={`/public/${imageName}`}
                 alt={`Icono de clima para ${weather && weather[0] && weather[0].description}`}
-            />
+            /> */}
             <p>
                 {parseInt(main.temp - kelvin)} <span>&#x2103;</span>
             </p>
